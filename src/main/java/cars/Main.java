789907b547a05312dd981a6c74ba9f6ccc483066
fdai2003 <fdai2003@gmail.com>
2024-02-23 16:16:32 +0100

    Initial commit

---
+++ src/main/java/cars/Main.java
@@ -0,0 +1,10 @@
+package cars;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Fahrzeug3Grün fahrzeugGrün = new Fahrzeug3Grün();
+        fahrzeugGrün.vorwärtsFahren();
+
+    }
+}
